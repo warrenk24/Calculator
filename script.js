f073b64b5108e48d5c64c0c90d5dd7c3414771fa
Khris <kaw0115@auburn.edu>
2022-12-31 22:31:01 -0600

    add a store number and operator function to display within the calc screen

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const operationButtons = document.querySelectorAll('[data-operation]')
 const deleteButtons = document.querySelectorAll('[data-delete]')
 const clearButtons = document.querySelectorAll('[data-clear]')
 const equalsButtons = document.querySelectorAll('[data-equals]')
-
+let currentNumber = ""
+let prevNumber= ""
+let currentOp = ""
 
 
 function add(num, num2) {
@@ -47,16 +49,34 @@ function operate(num, operand, num2) {
   }
 }
 
+function storeNumber(number) {
+    
+    if(currentNumber.length <= 12){
+        currentNumber += number
+        currentOperand.innerText = currentNumber 
+        
+    }
+    
+    
+    
+}
 
+
+function storeOperators(operator){
+    currentOp = operator
+    prevNumber = currentNumber
+    prevOperand.innerText = currentNumber + " " + operator
+    
+}
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        currentOperand.append(button.innerText)
+    button.addEventListener('click', (e) => {
+        storeNumber(e.target.innerText)
     })
 })
 
 operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        currentOperand.append(button.innerText)
+    button.addEventListener('click', (e) => {
+        storeOperators(e.target.innerText)
     })
 })
