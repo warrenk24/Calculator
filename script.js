35afc5f077f05724fc419b403bc5c82bbdd94da4
Khris <kaw0115@auburn.edu>
2023-01-01 09:45:22 -0600

    add decimal function

--- script.js
+++ script.js
@@ -1,100 +1,108 @@
-const prevOperand = document.querySelector('[data-prevOperand]')
-const currentOperand = document.querySelector('[data-currentOperand]')
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const deleteButtons = document.querySelector('[data-delete]')
-const clearButtons = document.querySelector('[data-clear]')
-clearButtons.addEventListener('click', clear)
-const equalsButtons = document.querySelector('[data-equals]')
-equalsButtons.addEventListener('click', operate)
-let currentNumber = ""
-let prevNumber= ""
-let currentOp = ""
-
+const prevOperand = document.querySelector("[data-prevOperand]");
+const currentOperand = document.querySelector("[data-currentOperand]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const deleteButtons = document.querySelector("[data-delete]");
+const decimalButtons = document.querySelector("[data-decimal]")
+decimalButtons.addEventListener('click', decimal)
+const clearButtons = document.querySelector("[data-clear]");
+clearButtons.addEventListener("click", clear);
+const equalsButtons = document.querySelector("[data-equals]");
+equalsButtons.addEventListener("click", operate);
+let currentNumber = "";
+let prevNumber = "";
+let currentOp = "";
 
 function add(num, num2) {
-    
-    prevNumber  =  num + num2;
-    currentOperand.innerText = prevNumber
+  prevNumber = num + num2;
+  currentOperand.innerText = prevNumber;
 }
 
 function subtract(num, num2) {
-    prevNumber =  num - num2;
-    currentOperand.innerText = prevNumber
+  prevNumber = num - num2;
+  currentOperand.innerText = prevNumber;
 }
 
 function multiply(num, num2) {
-    prevNumber =  num * num2;
-    currentOperand.innerText = prevNumber
-
+  prevNumber = num * num2;
+  currentOperand.innerText = prevNumber;
 }
 
 function divide(num, num2) {
-    prevNumber =  num / num2;
-    currentOperand.innerText = prevNumber
+  prevNumber = num / num2;
+  currentOperand.innerText = prevNumber;
 }
 
 function operate() {
-    prevNumber = Number(prevNumber)
-    currentNumber = Number(prevNumber)
-    
+  prevNumber = Number(prevNumber);
+  currentNumber = Number(currentNumber);
+
   switch (currentOp) {
     case "+":
-      return add(prevNumber, currentNumber);
+       add(prevNumber, currentNumber);
       break;
 
     case "-":
-      return subtract(prevNumber, prevNumber);
+       subtract(prevNumber, currentNumber);
       break;
 
     case "*":
-      return multiply(prevNumber, prevNumber);
+       multiply(prevNumber, currentNumber);
       break;
 
     case "/":
-      return divide(prevNumber, prevNumber);
+       divide(prevNumber, currentNumber);
       break;
 
     default:
       console.log("error");
   }
+ 
+  prevNumber = prevNumber.toString();
+  currentNumber = currentNumber.toString();
+  currentNumber = "";
+  
 }
 
 function storeNumber(number) {
-    
-    if(currentNumber.length <= 12){
-        currentNumber += number
-        currentOperand.innerText = currentNumber 
-        
-    }
- 
-    
-    
+  if (currentNumber.length <= 12) {
+    currentNumber += number;
+    currentOperand.innerText = currentNumber;
+  }
 }
 function clear() {
-
+   currentNumber = "";
+   prevNumber = "";
+   currentOp = "";
+   currentOperand.innerText = currentNumber;
+   prevOperand.innerText = prevNumber;
 }
 
-function storeOperators(operator){
-   
-    currentOp = operator
-    console.log(currentOp)
-    prevNumber = currentNumber
-    prevOperand.innerText = currentNumber + " " + operator
-    currentNumber = ""
-    currentOperand.innerText = ""
-    
+function storeOperators(operator) {
+  
+  currentOp = operator;
+  prevNumber = currentNumber;
+  prevOperand.innerText = currentNumber + " " + operator;
+  currentNumber = "";
+  currentOperand.innerText = "";
+ 
+}
+function decimal(){
+  if(!currentNumber.includes(".")){
+    currentOperand.innerText = currentNumber += "."
+  }
+  
+  
 }
 
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    storeNumber(e.target.innerText);
+  });
+});
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        storeNumber(e.target.innerText)
-    })
-})
-
-operationButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        storeOperators(e.target.innerText)
-    })
-})
+operationButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    storeOperators(e.target.innerText);
+  });
+});
